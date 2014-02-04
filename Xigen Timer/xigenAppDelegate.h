d41a4796e3dd5f4ff2020c7063df8a565969da42
Ben McCarthy <ben@xigen.co.uk>
2014-02-04 15:19:14 +0000

    Initial commit.

---
+++ Xigen Timer/xigenAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  xigenAppDelegate.h
+//  Xigen Timer
+//
+//  Created by Ben McCarthy on 04/02/2014.
+//  Copyright (c) 2014 Xigen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface xigenAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
