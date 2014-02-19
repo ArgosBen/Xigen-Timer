a59a038ca22e2d72683918dd612ecb4b672e8c22
Matt Malone <swapthatspoon@gmail.com>
2014-02-19 12:10:45 +0000

    Grunt, tidying up I think...

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    nodewebkit: {
+      options: {
+          build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+          mac: true, // We want to build it for mac
+          win: true, // We want to build it for win
+          linux32: true, // We don't need linux32
+          linux64: true // We don't need linux64
+      },
+      src: ['./package.json', './index.html', './node_modules/**/*', './assets/**/*'] // Your node-wekit app
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+  // Default task(s).
+  grunt.registerTask('default', ['nodewebkit']);
+
+};
