ef911a2158b7154f0d294bc0019a7799f10b1b44
Matt Malone <swapthatspoon@gmail.com>
2014-03-09 19:28:43 +0000

    Added grunt build task

--- grunt/notify.js
+++ grunt/notify.js
@@ -10,5 +10,11 @@ module.exports = {
             title: 'SASS Build Complete',  // optional
             message: 'SCSS built to assets/css/XigenTimer.css' //required
         }
+    },
+    nodewebkit: {
+        options: {
+            title: 'Compilation Complete',
+            message: 'Node-Webkit Build Success!'
+        }
     }
 };
