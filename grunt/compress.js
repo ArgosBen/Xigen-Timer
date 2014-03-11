fcdce4e108e77af7baaa2eaf522b884f155f52e6
Matt Malone <swapthatspoon@gmail.com>
2014-03-11 13:41:09 +0000

    Version number on zip files

--- grunt/compress.js
+++ grunt/compress.js
@@ -3,17 +3,19 @@
 	var grunt = require('grunt'),
 		isWin = grunt.file.isDir('webkitbuilds/releases/Xigen-EasyProjects-Timer/win/'),
 		isMac =  grunt.file.isDir('webkitbuilds/releases/Xigen-EasyProjects-Timer/mac/'),
-		isLinux = grunt.file.isDir('webkitbuilds/releases/Xigen-EasyProjects-Timer/linux32/');
+		isLinux = grunt.file.isDir('webkitbuilds/releases/Xigen-EasyProjects-Timer/linux32/'),
+		release = grunt.file.readJSON('package.json').version;
 
 	module.exports = {};
 
 	if (isWin) {
 		module.exports.win = {
 			options: {
-				archive: 'webkitbuilds/archives/XigenTimer_win.zip'
+				archive: 'webkitbuilds/archives/XigenTimer_win_' + release + '.zip'
 			},
 			files: {
-				src: ['webkitbuilds/releases/**/win/*', 'webkitbuilds/releases/**/win/**/*']
+				src: ['webkitbuilds/releases/**/win/*', 'webkitbuilds/releases/**/win/**/*'],
+				filter: 'isFile'
 			}
 		};
 	}
@@ -21,7 +23,7 @@
 	if (isMac) {
 		module.exports.mac = {
 			options: {
-				archive: 'webkitbuilds/archives/XigenTimer_osx.zip'
+				archive: 'webkitbuilds/archives/XigenTimer_osx_' + release + '.zip'
 			},
 			files: {
 				src: ['webkitbuilds/releases/**/mac/Xigen-EasyProjects-Timer.app']
@@ -32,7 +34,7 @@
 	if (isLinux) {
 		module.exports.linux32 = {
 			options: {
-				archive: 'webkitbuilds/archives/XigenTimer_linux32.zip'
+				archive: 'webkitbuilds/archives/XigenTimer_linux32_' + release + '.zip'
 			},
 			files: {
 				src: ['webkitbuilds/releases/**/linux32/*', 'webkitbuilds/releases/**/linux32/**/*']
@@ -41,7 +43,7 @@
 
 		module.exports.linux64 = {
 			options: {
-				archive: 'webkitbuilds/archives/XigenTimer_linux64.zip'
+				archive: 'webkitbuilds/archives/XigenTimer_linux64_' + release + '.zip'
 			},
 			files: {
 				src: ['webkitbuilds/releases/**/linux64/*', 'webkitbuilds/releases/**/linux64/**/*']
